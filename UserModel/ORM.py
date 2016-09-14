46a7d01bbbf0070bc29610aee37cbcb9a290d2bc
Denis Grebenets <dendendengrebenets@gmail.com>
2016-09-14 23:51:11 +0300

    PEP8 and PEP257 modidfiedversion of model

--- UserModel/ORM.py
+++ UserModel/ORM.py
@@ -1,39 +1,51 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""Give posibility of CRUD queries within methods.
+
+Simple ORM, that gives posibility of using CRUD queries within python methods.
+
+"""
+
 import MySQLdb
 
-class Driver(object):
 
-    """Class for describing sql commands with python"""
+class Driver(object):
+    """Describe SQL queries through methods.
+
+    Public methods: connect, close, execute_query, tuple_transform,
+    insert, select, update, delete.
+
+    """
 
     def connect(self, host, user_name, password, db):
-        """Connecting to database"""
+        """Connect to database."""
         self.db = MySQLdb.connect(host, user_name, password, db)
-
-        #Preparing using cursor() method
+        # Create cursor
         self.cursor = self.db.cursor(MySQLdb.cursors.Cursor)
 
+    def close(self):
+        """Close connection with database."""
+        self.db.close()
+
     def execute_query(self, query):
-        """Execute a sql query"""
+        """Execute a sql query."""
         try:
-            #Executing a sql query with execute() method
+            # Executing a sql query with execute() method
             self.cursor.execute(query)
 
-            #Finalizing the changes using commit() method
+            # Finalizing the changes using commit() method
             self.db.commit()
 
-            #Returning all fetched values
+            # Returning all fetched values
             self.cursor.fetchall()
         except:
-            #Rollback in case of error
+            # Rollback in case of error
             print "Error"
             self.db.rollback()
 
-
-
     def tuple_transform(self, _tuple):
-        """Transforms tuple into string"""
+        """Transform tuple into string."""
         loop_index = 1
         val = '('
         for field in _tuple:
@@ -48,65 +60,65 @@ class Driver(object):
             loop_index += 1
         val += ')'
 
-        return val      
+        return val
 
     def insert(self, table_name, columns, vals):
-        """Inserts new records in a table"""
+        """Insert new records in a table."""
         insert_query = "INSERT INTO %s (%s) VALUES %s" \
             % (table_name, (', ').join(columns), self.tuple_transform(vals))
-            
-        #Use execute_query() method
+
+        # Use execute_query() method
         self.execute_query(insert_query)
 
     def select(self, table_name, columns):
-        """Read the result of query"""
+        """Read the result of query."""
         select_query = "SELECT %s FROM %s" % ((', ').join(columns), table_name)
-        
+
         try:
-            #Executing query 
+            # Execute query
             self.cursor.execute(select_query)
 
-            #Fetching output of executing
+            # Fetch output of executing
             self.select_result = self.cursor.fetchall()
 
-            #Returning result
+            # Return result
             return self.select_result
         except:
-            #Rollback in case of error
+            # Rollback in case of error
             self.db.rollback()
 
     def update(self, table_name, changes, condition):
-        """Update records in a table"""
-        update_query = "UPDATE %s SET %s WHERE %s" % (table_name, changes, condition)
+        """Update records in a table."""
+        update_query = "UPDATE %s SET %s WHERE %s" % \
+            (table_name, changes, condition)
         self.execute_query(update_query)
 
     def delete(self, table_name, condition):
-        """Delete selected records"""
+        """Delete selected records."""
         delete_query = "DELETE FROM %s WHERE %s" % (table_name, condition)
-        self.execute_query(delete_query)        
+        self.execute_query(delete_query)
 
-#if __name__ == '__main__':
-    
-    #tmpDB = Driver()
+# if __name__ == '__main__':
 
-    #Connecting to database
-    #tmpDB.connect("localhost", "denny", "isurrender", "local_db")
+    # tmpDB = Driver()
 
-    
-    #print('Executing any query')
-    #tmpDB.execute_query('DROP TABLE IF EXISTS Test')
-    #tmpDB.execute_query('CREATE TABLE Test (Name VARCHAR(30), Age INT)')
+    # Connecting to database
+    # tmpDB.connect("localhost", "denny", "isurrender", "local_db")
 
-    #print('Inserting values')
-    #tmpDB.insert('Test', ('Name', 'Age'), ('Nick', 24))
+    # print('Executing any query')
+    # tmpDB.execute_query('DROP TABLE IF EXISTS Test')
+    # tmpDB.execute_query('CREATE TABLE Test (Name VARCHAR(30), Age INT)')
 
-    #print('Reading data')
-    #print tmpDB.select('Test', ('Name', 'Age'))    
-    
-    #print('Updating data')
-    #tmpDB.update('EMPLOYEE', 'FIRST_NAME = "Denis"', 'LAST_NAME = "Mohan"')
+    # print('Inserting values')
+    # tmpDB.insert('Test', ('Name', 'Age'), ('Nick', 24))
 
-    #print('Deleting data')
-    #tmpDB.delete('EMPLOYEE', 'AGE = 21')
+    # print('Reading data')
+    # print tmpDB.select('Test', ('Name', 'Age'))
 
-    #tmpDB.db.close()
+    # print('Updating data')
+    # tmpDB.update('EMPLOYEE', 'FIRST_NAME = "Denis"', 'LAST_NAME = "Mohan"')
+
+    # print('Deleting data')
+    # tmpDB.delete('EMPLOYEE', 'AGE = 21')
+
+    # tmpDB.close()
--- UserModel/UModel.py
+++ UserModel/UModel.py
@@ -1,57 +1,78 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import MySQLdb
+"""Describe model of user.
 
-from flask import Flask
+Model of user, that gives posibility of using CRUD queries for user.
+
+"""
 
 from ORM import Driver
 
+
 class UserModel(Driver):
+    """Give posibility of using CRUD queries for user.
+
+    UserModel extends Driver methods.
+
+    Instance variables: table_name.
+
+    Public methods: connect_to_db, insert_user,
+    select_user, update_user, delete_user.
+
+    """
 
     def __init__(self):
+        """Initialize variable and object."""
         self.table_name = "Test"
         self.DB = Driver()
 
-    def connect_to_db(self):
-        self.DB.connect("localhost", "denny", "isurrender", "local_db")
-
-    #def create_user_table(self):
-    #   print('Creating Table')
-    #   self.connect_to_db()
-        #self.DB.execute_query('DROP TABLE IF EXISTS Test')
-        #self.DB.execute_query('CREATE TABLE Test (id INT, fullName VARCHAR(100), email VARCHAR(100), password VARCHAR(10), avatar VARCHAR(100), isActive BOOLEAN, role_id INT, PRIMARY KEY(id))')
+    def connect_to_db(self, host, user_name, password, db):
+        """Connect to database."""
+        self.DB.connect(host, user_name, password, db)
 
     def insert_user(self, columns, vals):
+        """Insert records into user table."""
         print "Inserting"
         self.DB.insert(self.table_name, columns, vals)
 
     def select_user(self, columns):
+        """Read user records."""
         print "Reading"
         print self.DB.select(self.table_name, columns)
 
     def update_user(self, changes, condition):
+        """Update user records."""
         print "Updating"
         self.DB.update(self.table_name, changes, condition)
 
     def delete_user(self, condition):
+        """Delete user."""
         print "Deleting"
         self.DB.delete(self.table_name, condition)
 
 if __name__ == '__main__':
-    
+
     User = UserModel()
-    
+
     print('Creating Table')
-    User.connect_to_db()
-    
-    #User.DB.execute_query('DROP TABLE IF EXISTS Test')
-    #User.DB.execute_query('CREATE TABLE Test (id INT NOT NULL AUTO_INCREMENT, fullName VARCHAR(100), email VARCHAR(100), password VARCHAR(10), avatar VARCHAR(100), isActive BOOLEAN, role_id INT, PRIMARY KEY(id))')
+    User.connect_to_db("localhost", "denny", "isurrender", "local_db")
 
-    #User.insert_user(('fullName', 'email', 'password', 'avatar', 'isActive', 'role_id'), ('Oleg', 'naumleg@urk.net', 'qweasdzxc', 'Uleshka', 1, 2))
+    User.DB.execute_query('DROP TABLE IF EXISTS Test')
+    User.DB.execute_query('CREATE TABLE Test (id INT NOT NULL AUTO_INCREMENT, '
+                          'fullName VARCHAR(100), email VARCHAR(100), '
+                          'password VARCHAR(10), '
+                          'avatar VARCHAR(100), isActive BOOLEAN, '
+                          'role_id INT, PRIMARY KEY(id))')
 
-    #User.select_user(('fullName', 'email'))
+    User.insert_user(('fullName', 'email', 'password', 'avatar',
+                     'isActive', 'role_id'), ('Oleg', 'naumleg@urk.net',
+                     'qweasdzxc', 'Uleshka', 1, 2))
 
-    #User.update_user('avatar = "Naumchuk"', 'fullName = "Oleg"')
+    User.select_user(('fullName', 'email'))
 
-    #User.delete_user('password = "hahaha"')
+    User.update_user('avatar = "Naumchuk"', 'fullName = "Oleg"')
+
+    # User.delete_user('password = "hahaha"')
+
+    User.DB.close()
