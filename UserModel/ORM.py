968397147979e838cd0d97738100a7ca845fed8c
Denis Grebenets <dendendengrebenets@gmail.com>
2016-09-14 19:30:52 +0300

    Configurate gitignore file

--- UserModel/ORM.py
+++ UserModel/ORM.py
@@ -5,108 +5,108 @@ import MySQLdb
 
 class Driver(object):
 
-	"""Class for describing sql commands with python"""
+    """Class for describing sql commands with python"""
 
-	def connect(self, host, user_name, password, db):
-		"""Connecting to database"""
-		self.db = MySQLdb.connect(host, user_name, password, db)
+    def connect(self, host, user_name, password, db):
+        """Connecting to database"""
+        self.db = MySQLdb.connect(host, user_name, password, db)
 
-		#Preparing using cursor() method
-		self.cursor = self.db.cursor(MySQLdb.cursors.Cursor)
+        #Preparing using cursor() method
+        self.cursor = self.db.cursor(MySQLdb.cursors.Cursor)
 
-	def execute_query(self, query):
-		"""Execute a sql query"""
-		try:
-			#Executing a sql query with execute() method
-			self.cursor.execute(query)
+    def execute_query(self, query):
+        """Execute a sql query"""
+        try:
+            #Executing a sql query with execute() method
+            self.cursor.execute(query)
 
-			#Finalizing the changes using commit() method
-			self.db.commit()
+            #Finalizing the changes using commit() method
+            self.db.commit()
 
-			#Returning all fetched values
-			self.cursor.fetchall()
-		except:
-			#Rollback in case of error
-			print "Error"
-			self.db.rollback()
+            #Returning all fetched values
+            self.cursor.fetchall()
+        except:
+            #Rollback in case of error
+            print "Error"
+            self.db.rollback()
 
 
 
-	def tuple_transform(self, _tuple):
-		"""Transforms tuple into string"""
-		loop_index = 1
-		val = '('
-		for field in _tuple:
-			if isinstance(field, int):
-				val += str(field)
-				if loop_index < len(_tuple):
-					val += ', '
-			elif isinstance(field, str):
-				val += '"' + field + '"'
-				if loop_index < len(_tuple):
-					val += ', '
-			loop_index += 1
-		val += ')'
+    def tuple_transform(self, _tuple):
+        """Transforms tuple into string"""
+        loop_index = 1
+        val = '('
+        for field in _tuple:
+            if isinstance(field, int):
+                val += str(field)
+                if loop_index < len(_tuple):
+                    val += ', '
+            elif isinstance(field, str):
+                val += '"' + field + '"'
+                if loop_index < len(_tuple):
+                    val += ', '
+            loop_index += 1
+        val += ')'
 
-		return val		
+        return val      
 
-	def insert(self, table_name, columns, vals):
-		"""Inserts new records in a table"""
-		insert_query = "INSERT INTO %s (%s) VALUES %s" \
-			% (table_name, (', ').join(columns), self.tuple_transform(vals))
-			
-		#Use execute_query() method
-		self.execute_query(insert_query)
+    def insert(self, table_name, columns, vals):
+        """Inserts new records in a table"""
+        insert_query = "INSERT INTO %s (%s) VALUES %s" \
+            % (table_name, (', ').join(columns), self.tuple_transform(vals))
+            
+        #Use execute_query() method
+        self.execute_query(insert_query)
 
-	def select(self, table_name, columns):
-		"""Read the result of query"""
-		select_query = "SELECT %s FROM %s" % ((', ').join(columns), table_name)
-		
-		try:
-			#Executing query 
-			self.cursor.execute(select_query)
+    def select(self, table_name, columns):
+        """Read the result of query"""
+        select_query = "SELECT %s FROM %s" % ((', ').join(columns), table_name)
+        
+        try:
+            #Executing query 
+            self.cursor.execute(select_query)
 
-			#Fetching output of executing
-			self.select_result = self.cursor.fetchall()
+            #Fetching output of executing
+            self.select_result = self.cursor.fetchall()
 
-			#Returning result
-			return self.select_result
-		except:
-			#Rollback in case of error
-			self.db.rollback()
+            #Returning result
+            return self.select_result
+        except:
+            #Rollback in case of error
+            self.db.rollback()
 
-	def update(self, table_name, changes, condition):
-		"""Update records in a table"""
-		update_query = "UPDATE %s SET %s WHERE %s" % (table_name, changes, condition)
-		self.execute_query(update_query)
+    def update(self, table_name, changes, condition):
+        """Update records in a table"""
+        update_query = "UPDATE %s SET %s WHERE %s" % (table_name, changes, condition)
+        self.execute_query(update_query)
 
-	def delete(self, table_name, condition):
-		"""Delete selected records"""
-		delete_query = "DELETE FROM %s WHERE %s" % (table_name, condition)
-		self.execute_query(delete_query)		
+    def delete(self, table_name, condition):
+        """Delete selected records"""
+        delete_query = "DELETE FROM %s WHERE %s" % (table_name, condition)
+        self.execute_query(delete_query)        
 
 #if __name__ == '__main__':
-	
-	#tmpDB = Driver()
+    
+    #tmpDB = Driver()
 
-	#Connecting to database
-	#tmpDB.connect("localhost", "denny", "isurrender", "local_db")
+    #Connecting to database
+    #tmpDB.connect("localhost", "denny", "isurrender", "local_db")
 
-	
-	#print('Executing any query')
-	#tmpDB.execute_query('DROP TABLE IF EXISTS Test')
-	#tmpDB.execute_query('CREATE TABLE Test (Name VARCHAR(30), Age INT)')
+    
+    #print('Executing any query')
+    #tmpDB.execute_query('DROP TABLE IF EXISTS Test')
+    #tmpDB.execute_query('CREATE TABLE Test (Name VARCHAR(30), Age INT)')
 
-	#print('Inserting values')
-	#tmpDB.insert('Test', ('Name', 'Age'), ('Nick', 24))
+    #print('Inserting values')
+    #tmpDB.insert('Test', ('Name', 'Age'), ('Nick', 24))
 
-	#print('Reading data')
-	#print tmpDB.select('Test', ('Name', 'Age'))	
-	
-	#print('Updating data')
-	#tmpDB.update('EMPLOYEE', 'FIRST_NAME = "Denis"', 'LAST_NAME = "Mohan"')
+    #print('Reading data')
+    #print tmpDB.select('Test', ('Name', 'Age'))    
+    
+    #print('Updating data')
+    #tmpDB.update('EMPLOYEE', 'FIRST_NAME = "Denis"', 'LAST_NAME = "Mohan"')
 
-	#print('Deleting data')
-	#tmpDB.delete('EMPLOYEE', 'AGE = 21')
+    #print('Deleting data')
+    #tmpDB.delete('EMPLOYEE', 'AGE = 21')
 
-	#tmpDB.db.close()
+    #tmpDB.db.close()
--- UserModel/UModel.py
+++ UserModel/UModel.py
@@ -1,57 +1,57 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-
 import MySQLdb
+
+from flask import Flask
 
 from ORM import Driver
 
 class UserModel(Driver):
 
-	def __init__(self):
-		self.table_name = "Test"
-		self.DB = Driver()
+    def __init__(self):
+        self.table_name = "Test"
+        self.DB = Driver()
 
-	def connect_to_db(self):
-		self.DB.connect("localhost", "denny", "isurrender", "local_db")
+    def connect_to_db(self):
+        self.DB.connect("localhost", "denny", "isurrender", "local_db")
 
-	#def create_user_table(self):
-	#	print('Creating Table')
-	#	self.connect_to_db()
-		#self.DB.execute_query('DROP TABLE IF EXISTS Test')
-		#self.DB.execute_query('CREATE TABLE Test (id INT, fullName VARCHAR(100), email VARCHAR(100), password VARCHAR(10), avatar VARCHAR(100), isActive BOOLEAN, role_id INT, PRIMARY KEY(id))')
+    #def create_user_table(self):
+    #   print('Creating Table')
+    #   self.connect_to_db()
+        #self.DB.execute_query('DROP TABLE IF EXISTS Test')
+        #self.DB.execute_query('CREATE TABLE Test (id INT, fullName VARCHAR(100), email VARCHAR(100), password VARCHAR(10), avatar VARCHAR(100), isActive BOOLEAN, role_id INT, PRIMARY KEY(id))')
 
-	def insert_user(self, columns, vals):
-		print "Inserting"
-		self.DB.insert(self.table_name, columns, vals)
+    def insert_user(self, columns, vals):
+        print "Inserting"
+        self.DB.insert(self.table_name, columns, vals)
 
-	def select_user(self, columns):
-		print "Reading"
-		print self.DB.select(self.table_name, columns)
+    def select_user(self, columns):
+        print "Reading"
+        print self.DB.select(self.table_name, columns)
 
-	def update_user(self, changes, condition):
-		print "Updating"
-		self.DB.update(self.table_name, changes, condition)
+    def update_user(self, changes, condition):
+        print "Updating"
+        self.DB.update(self.table_name, changes, condition)
 
-	def delete_user(self, condition):
-		print "Deleting"
-		self.DB.delete(self.table_name, condition)
+    def delete_user(self, condition):
+        print "Deleting"
+        self.DB.delete(self.table_name, condition)
 
 if __name__ == '__main__':
-	
-	User = UserModel()
-	
-	print('Creating Table')
-	User.connect_to_db()
-	
-	#User.DB.execute_query('DROP TABLE IF EXISTS Test')
-	#User.DB.execute_query('CREATE TABLE Test (id INT NOT NULL AUTO_INCREMENT, fullName VARCHAR(100), email VARCHAR(100), password VARCHAR(10), avatar VARCHAR(100), isActive BOOLEAN, role_id INT, PRIMARY KEY(id))')
+    
+    User = UserModel()
+    
+    print('Creating Table')
+    User.connect_to_db()
+    
+    #User.DB.execute_query('DROP TABLE IF EXISTS Test')
+    #User.DB.execute_query('CREATE TABLE Test (id INT NOT NULL AUTO_INCREMENT, fullName VARCHAR(100), email VARCHAR(100), password VARCHAR(10), avatar VARCHAR(100), isActive BOOLEAN, role_id INT, PRIMARY KEY(id))')
 
-	#User.insert_user(('fullName', 'email', 'password', 'avatar', 'isActive', 'role_id'), ('Oleg', 'naumleg@urk.net', 'qweasdzxc', 'Uleshka', 1, 2))
+    #User.insert_user(('fullName', 'email', 'password', 'avatar', 'isActive', 'role_id'), ('Oleg', 'naumleg@urk.net', 'qweasdzxc', 'Uleshka', 1, 2))
 
-	#User.select_user(('fullName', 'email'))
+    #User.select_user(('fullName', 'email'))
 
-	#User.update_user('avatar = "Naumchuk"', 'fullName = "Oleg"')
+    #User.update_user('avatar = "Naumchuk"', 'fullName = "Oleg"')
 
-	#User.delete_user('password = "hahaha"')
+    #User.delete_user('password = "hahaha"')
